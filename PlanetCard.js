a58d3b8453889518bcdaac6aa0b0c12d352cc185
Jéssica Pereira <88255575+jessnay@users.noreply.github.com>
2022-03-28 12:04:31 -0300

    Add files via upload

---
+++ PlanetCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div className="planet-card" data-testid="planet-card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p className="name-planet" data-testid="planet-name">{ planetName }</p>
+      </div>
+
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+}.isRequired;
+
+export default PlanetCard;
